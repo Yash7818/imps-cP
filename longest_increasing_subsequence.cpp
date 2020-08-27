a539575742049ca4f35b4d46f44c174ba4f2d91a
unknown <yashwandhare1234@gmail.com>
2020-08-28 01:20:15 +0530

    updated

--- longest_increasing_subsequence.cpp
+++ longest_increasing_subsequence.cpp
@@ -2,8 +2,9 @@
 #define ll long long int
 #define rep(i,n) for(ll i=0;i<n;i++)
 using namespace std;
+ll max_ind,li[100000];
 ll lis(ll *a,ll n){
-	ll li[n],i,j;
+	ll i,j;
 	for(i=0;i<n;i++)
 	li[i] = 1;
 	
@@ -17,11 +18,27 @@ ll lis(ll *a,ll n){
 	ll max = 0;
 	for(i=0;i<n;i++){
 		if(max<li[i])
-		max = li[i];
+		max = li[i],max_ind = i;
 //		cout<<li[i]<<" ";
 	}
 //	cout<<endl;
 	return max;
+}
+void print_it(ll *a,ll n){
+	ll max = lis(a,n);
+//	cout<<max_ind;
+	vector<ll> v;
+	v.push_back(a[max_ind]);
+	for(ll i=max_ind-1;i>=0;i--){
+		if(li[i]+1==li[max_ind] && a[i]<a[max_ind]){
+			v.push_back(a[i]);
+			max_ind = i;
+		}
+	}
+	reverse(v.begin(),v.end());
+	for(auto x:v)
+	cout<<x<<" ";
+	
 }
 int main(){
 	ll n;
@@ -29,6 +46,7 @@ int main(){
 	ll a[n];
 	for(ll i=0;i<n;i++) cin>>a[i];
 	
-	cout<<lis(a,n);
+	cout<<lis(a,n)<<endl;
+	print_it(a,n);
 	return 0;
 }
